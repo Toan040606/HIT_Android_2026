02f44541751ef03048910b3ef55105a6b889f19c
Toan040606 <toannickson2k60604@gmail.com>
2026-03-28 17:31:31 +0700

    Lession 2: Calculator Layout

---
+++ Calculator/app/src/main/java/com/calculator/Calculator.kt
@@ -0,0 +1,24 @@
+package com.calculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.calculator.databinding.ActivityCalculatorBinding
+
+class Calculator : AppCompatActivity() {
+    private lateinit var binding: ActivityCalculatorBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
