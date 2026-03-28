b5e30ec7c08ec13af01bf39db2e1b9b200df3449
Toan040606 <toannickson2k60604@gmail.com>
2026-03-28 17:29:31 +0700

    Lession 2: Calculator Layout

---
+++ Calculator/app/src/main/java/com/calculator/ChangeTheme.kt
@@ -0,0 +1,24 @@
+package com.calculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.calculator.databinding.ActivityChangeThemeBinding
+
+class ChangeTheme : AppCompatActivity() {
+    private lateinit var binding: ActivityChangeThemeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityChangeThemeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- LoginLayout/app/src/main/java/root/loginlayout/MainActivity.kt
+++ LoginLayout/app/src/main/java/root/loginlayout/MainActivity.kt
@@ -1,20 +1,35 @@
 package root.loginlayout
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import root.loginlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val loginBtn = binding.loginBtn
+        loginBtn.setOnClickListener {
+            val email = binding.emailInp.text.toString()
+            val password = binding.passInp.text.toString()
+            Log.d("abc", "Email: $email, Password: $password")
+        }
+
     }
 }
